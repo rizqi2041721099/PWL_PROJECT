412cb01ef6b499104d51bc121ae335947bd261b1
rizqi2041720199 <rizqiahmad939@gmail.com>
2022-06-27 11:28:22 +0700

    sync

--- resources/views/pages/peminjaman/index.blade.php
+++ resources/views/pages/peminjaman/index.blade.php
@@ -87,7 +87,7 @@
                        @endforeach
                    </tbody>
                </table>
-               {{ $data->links() }}
+              
            </div>
        </div>
    </div>
--- resources/views/pages/pengembalian/index.blade.php
+++ resources/views/pages/pengembalian/index.blade.php
@@ -3,43 +3,11 @@
 <div class="container-fluid">
 
    <!-- Page Heading -->
-   <h1 class="h3 mb-2 text-gray-800">Data Buku</h1>
+   <h1 class="h3 mb-2 text-gray-800">Data Pengembalian</h1>
    <!-- DataTales Example -->
    <div class="card shadow mb-4">
        <div class="card-header py-3">
-        {{-- @can('books-create')
-            <a href="{{route('pengembalian.create')}}" class="btn btn-success btn-icon-split">
-                <span class="icon text-white-50">
-                    <i class="fas fa-plus"></i>
-                </span>
-                <span class="text">Tambah Buku</span>
-            </a>
-        @endcan --}}
-        @if(session('success'))
-            <div class="alert alert-success alert-dismissible mt-4" role="alert">
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-                <h6><i class="fas fa-check"></i><b> Success  {{session('success')}}</b></h6>
-              </div>
-            @endif
-
-           @if(session('delete'))
-            <div class="alert alert-danger alert-dismissible mt-4" role="alert">
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-                <h6><i class="fas fa-trash"></i><b>  {{session('delete')}}</b></h6>
-              </div>
-          @endif
-          @if(session('warning'))
-            <div class="alert alert-warning alert-dismissible mt-4" role="alert">
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-                <h6><i class="fas fa-trash"></i><b>  {{session('warning')}}</b></h6>
-              </div>
-           @endif
+       
        </div>
        <div class="card-body">
            <div class="table-responsive">
